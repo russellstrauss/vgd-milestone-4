15c989ebf3ff0bb4f4a6adeb5d6b2f2d82db40d1
Russell Strauss <russellstrauss@gmail.com>
2020-06-19 18:06:40 -0400

    Set camera

---
+++ Assets/Scripts/MinionAIController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinionAIController : MonoBehaviour
+{
+	
+	public GameObject[] waypoints;
+	private int currentWaypoint;
+	
+	void Start() {
+		
+	}
+
+	void Update() {
+		
+	}
+	
+	void setNextWaypoint() {
+		
+	}
+	
+	void setDestination() {
+		
+	}
+}
