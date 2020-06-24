93fe6809b99bfe1b0d5f6c27467332c4896526b7
Russell Strauss <russellstrauss@gmail.com>
2020-06-24 18:12:18 -0400

    add final details

--- Assets/Scripts/MinionAIController.cs
+++ Assets/Scripts/MinionAIController.cs
@@ -7,7 +7,9 @@ public class MinionAIController : MonoBehaviour
 {
 	public NavMeshAgent agent;
 	public GameObject[] waypoints;
+	public GameObject[] movingWaypoints;
 	private int currentWaypoint = 0;
+	private int currentMovingWaypoint = 0;
 	Animator animator;
 	
 	public AIState aiState;
@@ -18,9 +20,10 @@ public class MinionAIController : MonoBehaviour
 	
 	void Start() {
 		animator = gameObject.GetComponent<Animator>();
-		//aiState = AIState.GoToMovingWaypoint;
+		aiState = AIState.GoToMovingWaypoint;
 		
-		if (aiState == AIState.Patrol) setNextWaypoint();
+		if (aiState == AIState.Patrol) setNextStaticWaypoint();
+		//else if (aiState == AIState.GoToMovingWaypoint) setNextMovingWaypoint();
 	}
 
 	void Update() {
@@ -28,24 +31,42 @@ public class MinionAIController : MonoBehaviour
 		switch (aiState) {
 			case AIState.Patrol:
 				animator.SetFloat("vely", agent.velocity.magnitude / agent.speed);
-				if (agent.remainingDistance < 2 && !agent.pathPending) setNextWaypoint();
+				if ((agent.remainingDistance - agent.stoppingDistance < 2) && !agent.pathPending) setNextStaticWaypoint();
 			break;
 			case AIState.GoToMovingWaypoint:
-				Debug.Log("MovingWaypoint State");
+				animator.SetFloat("vely", agent.velocity.magnitude / agent.speed);
+				if ((agent.remainingDistance < 1) && !agent.pathPending) {
+					setNextMovingWaypoint();
+				}
 			break;
 			default:
 			break;
 		}
 	}
 	
-	void setNextWaypoint() {
+	void setNextStaticWaypoint() {
 		if (currentWaypoint > waypoints.Length - 1) currentWaypoint = 0;
-		setDestination();
+		Vector3 destination = waypoints[currentWaypoint].transform.position;
+		setDestination(destination);
 		currentWaypoint++;
+		aiState = AIState.GoToMovingWaypoint;
 	}
 	
-	void setDestination() {
-		Vector3 destination = waypoints[currentWaypoint].transform.position;
+	void setNextMovingWaypoint() {
+		if (currentMovingWaypoint > movingWaypoints.Length - 1) {
+			currentMovingWaypoint = 0;
+		}
+		Vector3 destination = movingWaypoints[currentMovingWaypoint].transform.position;
+		float distance = (destination - agent.transform.position).magnitude;
+		float lookAheadPosition = distance / agent.speed;
+		Rigidbody rb = movingWaypoints[currentMovingWaypoint].GetComponent<Rigidbody>();
+		Vector3 futureDestination = destination + lookAheadPosition * rb.velocity;
+		setDestination(destination);
+		currentMovingWaypoint++;
+		aiState = AIState.Patrol;
+	}
+	
+	void setDestination(Vector3 destination) {
 		agent.SetDestination(destination);
 	}
 }
--- strauss_r_m4_readme.txt
+++ strauss_r_m4_readme.txt
@@ -3,25 +3,7 @@ jstrauss6@gatech.edu
 
 
 Checklist																										Implemented
-	• Working Game Start Menu with dedicated scene (25 points)														• Yes, all
-		o Overlay UI Panel (Start Menu)
-			§ Start Game button
-			§ Exit button
-		o Camera background																							• Added anti-aliasing to reduce hard pixel edges, especially notable on shadows
-			§ Post-processing effect on the camera
-	• Working In-Game Menu in original gameplay scene (25 points)													• Yes, all
-		o Overlay UI Panel
-			§ Restart Game button
-			§ Exit button
-		o Panel responds toggles on/off with Escape key
-		o Game pauses when menu enabled
-	• Collectable ball that only SomeDude_RootMotion can collect (20 points)										• Yes, large bink ball
-	• Trigger-based animated prefab object placed in three (3) locations in scene (30 points)						• Yes, spinning black cubes speed up when in proximity (humanoid character only)
-		o Object is prefabbed
-		o Object animates in a compelling way when player gets close enough
-		o Object resets when player is far enough away (if obj in triggered state)
-	• Consider doing the extra credit below																			• Extra credit not implemented
-	• Build																											• Yes, build located \vgd-milestone-3\Builds\strauss_r_m3.exe
-	
-	
-																													• All required elements complete
+1.) AI controlled Minion visits 5 stationary waypoints (30 pts)													Yes
+2.) AI controlled Minion heads off moving waypoint (30 pts)														Yes
+3.) AI is controlled by procedural state machine of at least 2 states (20 pts)									Yes
+4.) Minion is animated with steps and not sunk into ground (20 pts												Yes
